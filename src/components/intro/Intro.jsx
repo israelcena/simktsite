42b8eaf6c0a836a7810a169e1b1d04505924c105
Israel Cena <israelsjm@gmail.com>
2022-10-22 00:30:05 -0300

    include intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,37 @@
 import "./intro.scss"
+import mainImage from "../../images/salesperson.png"
+import { IoIosArrowDown } from "react-icons/io";
+import { init } from 'ityped'
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, { 
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['redes sociais.','sistemas internos.', 'sistemas externos.', 'campanhas de marketing', 'público alvo' ] 
+    })
+  },[]);
   return (
-    <div className="intro" id="intro">Intro</div>
+    <main className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src={mainImage} alt="SI Marketing Client" />
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="wrapper">
+          <h2>Olá, Somos a</h2>
+          <h1>SI Marketing</h1>
+          <h3><span>Cuidamos</span> da sua empresa<br/>em <span ref={textRef}></span></h3>
+        </div>
+        <a href="#portfolio"><IoIosArrowDown/></a>
+      </div>
+    </main>
   )
 }
