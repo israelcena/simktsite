15c39ed909b820356652472708ea7500477a304f
Israel Cena <israelsjm@gmail.com>
2022-11-23 09:48:12 -0300

    include new content

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -9,34 +9,34 @@ export default function Testimonials() {
   const data = [
     {
       id: 1,
-      name: "Tom Durden",
+      name: "Israel Cena",
       title: "Senior Developer",
-      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://pbs.twimg.com/profile_images/1335683030007111682/l9D6BDzO_400x400.jpg",
       icon: <AiFillTwitterCircle style={{ color: `#00acee` }} />,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+      desc: "Desenvolvedor desde 2007 Formado em Marketing e Sistemas de Informação Ajuda em projetos Open Source e em Ongs.",
     },
     {
       id: 2,
-      name: "Alex Kalinski",
-      title: "Co-Founder of DELKA",
+      name: "Sara Nunes",
+      title: "Co-Founder",
       img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       icon: <AiFillYoutube style={{ color: `#FF0000` }} />,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+      desc: "Formada em administração de empresas administra e executa ações de marketing intenas e externas.",
       featured: true,
     },
     {
       id: 3,
-      name: "Martin Harold",
-      title: "CEO of ALBI",
+      name: "Dilton Neves",
+      title: "Especialista em Marketing",
       img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
       icon: <AiFillLinkedin style={{ color: `#0e76a8` }} />,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+      desc: "Focado em satisfação do cliente e campanhas de marketing.",
     },
   ];
 
   return (
     <section className="testimonials" id="testimonials">
-      <h2>Depoimentos</h2>
+      <h2>Equipe</h2>
       <div className="container" onDrag={()=>{console.log('hei')}}>
         {data ?
           data.map(({ id, desc, featured, icon, img, name, title }) => (
